8477021a54da68d93c34c174dd29912867f3f65e
yuvrajcet26-dotcom <yuvrajcet26@gmail.com>
2026-06-03 14:54:55 +0000

    day2 variable completed

---
+++ day2/variable.js
@@ -0,0 +1,12 @@
+const accountid = 1425
+let accountEmail="yuvraj24@gmail.com" 
+var accountPassword="yuvraj@123"
+console.log(accountid)
+accountEmail="yp@s.com"
+accountPassword="yuvraj@1234"
+accountCity="Bangalore"
+console.table([accountid,accountEmail,accountPassword,accountCity]) 
+// accountid=1426
+/*
+Prefer not to use var and use let and const instead of var because of the scope issues with var.
+*/
