64c1eb5d8e6eb4a0fae2e08701cf4208888e84a5
yuvrajcet26-dotcom <yuvrajcet26@gmail.com>
2026-06-05 08:37:02 +0000

    day4 completed

---
+++ day4/compare.js
@@ -0,0 +1,21 @@
+// Operators : >,<,>=,<=,==,===
+
+console.log(1>2)
+console.log(1==2)
+console.log(1===2)
+console.log(1>=2)
+console.log(1<=2)
+console.log("143"===143)
+console.log("143"==143)
+
+//Special Cases :
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) //Numeric Coercion (Number(null)=0)
+
+
+// False :
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
